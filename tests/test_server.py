f99b732d3d5567bb957362ecf331ba96683048b9
Gabriel Pettier <gabriel@tangibledisplay.com>
2018-04-02 02:37:35 +0200

    add test for decorator api

--- tests/test_server.py
+++ tests/test_server.py
@@ -36,3 +36,25 @@ def test_bind():
     while not cont:
         if time() > timeout:
             raise OSError('timeout while waiting for  success message.')
+
+
+def test_decorator():
+    osc = OSCThreadServer()
+    sock = osc.listen()
+    port = sock.getsockname()[1]
+    cont = []
+
+    @osc.address(sock, '/success')
+    def success(*values):
+        cont.append(True)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(
+        format_message(b'/success', [b"test", b"test", 1, 1.2345]),
+        ('localhost', port)
+    )
+
+    timeout = time() + 5
+    while not cont:
+        if time() > timeout:
+            raise OSError('timeout while waiting for  success message.')
