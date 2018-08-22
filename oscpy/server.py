60fa92bb5d23c4de0f12e7966f02df11c208e3f8
RC <cjr.chang@gmail.com>
2018-08-22 17:53:30 +1000

    Partial fix for specifying port in response.
    Still not fixed in unix family (socket.AF_UNIX).

--- oscpy/server.py
+++ oscpy/server.py
@@ -325,7 +325,7 @@ class OSCThreadServer(object):
         send_bundle(messages, ip_address, port, sock=sock, safer=safer)
 
     def answer(
-        self, address=None, values=None, bundle=None, timetag=None, safer=False
+        self, address=None, values=None, bundle=None, timetag=None, safer=False, port=None
     ):
         '''Answers a message or bundle to a client
         this method can only be called from a callback, it will lookup
@@ -348,16 +348,18 @@ class OSCThreadServer(object):
         else:
             raise RuntimeError('answer() not called from a callback')
 
-        ip_address, port = frame.f_locals.get('sender')
+        ip_address, responseport = frame.f_locals.get('sender')
+        if port != None:
+            responseport = port
         sock = frame.f_locals.get('sender_socket')
 
         if bundle:
             self.send_bundle(
-                bundle, ip_address, port, timetag=timetag, sock=sock,
+                bundle, ip_address, responseport, timetag=timetag, sock=sock,
                 safer=safer
             )
         else:
-            self.send_message(address, values, ip_address, port, sock=sock)
+            self.send_message(address, values, ip_address, responseport, sock=sock)
 
     def address(self, address, sock=None):
         '''Decorator method to allow binding functions from their definition
