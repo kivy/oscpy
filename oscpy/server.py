c60235fdafd8b9509877aad1287f01a993eaa4ee
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-05-27 00:33:21 +0200

    tentative fix for #31

--- oscpy/server.py
+++ oscpy/server.py
@@ -42,6 +42,9 @@ def ServerClass(cls):
     return cls
 
 
+__FILE__ = inspect.getfile(ServerClass)
+
+
 class OSCThreadServer(object):
     """A thread-based OSC server.
 
@@ -442,7 +445,7 @@ class OSCThreadServer(object):
         """
         frames = inspect.getouterframes(inspect.currentframe())
         for frame, filename, _, function, _, _ in frames:
-            if function == '_listen' and __file__.startswith(filename):
+            if function == '_listen' and __FILE__.startswith(filename):
                 break
         else:
             raise RuntimeError('get_sender() not called from a callback')
