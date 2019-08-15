fb8bd914118f050336bab3b2a8b2e86d0d3e2c87
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 19:51:34 +0200

    fix twine call

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -4,11 +4,11 @@ set -ex
 if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     pip install -U setuptools wheel twine
     python setup.py sdist bdist_wheel
-    twine check dist/*
+    python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
-    twine upload --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
+    python -m twine upload --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
     echo "OOF this would have uploaded"
-    # twine upload --disable-progress-bar dist/*
+    # python -m twine upload --disable-progress-bar dist/*
   fi
 fi
