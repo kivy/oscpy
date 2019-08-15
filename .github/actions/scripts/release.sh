e3cc7293d91305cc1368903e402c203d22a81e96
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 20:34:17 +0200

    use twine test token

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -6,7 +6,7 @@ if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     python setup.py sdist bdist_wheel
     python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
-    python -m twine upload -u oscpy --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
+    python -m twine upload -u oscpy -p $TWINE_PASSWORD_TEST --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
     echo "OOF this would have uploaded"
     # python -m twine upload -u oscpy --disable-progress-bar dist/*
