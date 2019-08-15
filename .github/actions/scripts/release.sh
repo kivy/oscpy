7c43931b5eb2782b92ee75004b86b883a3cf7e4a
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 23:00:47 +0200

    commented line

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -6,7 +6,6 @@ if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     python setup.py sdist bdist_wheel
     python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
-    TWINE_PASSWORD=$TWINE_PASSWORD_TEST
     python -m twine upload -u oscpy --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
     python -m twine upload -u oscpy --disable-progress-bar dist/*
