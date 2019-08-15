ad6bcf101cda27d9077e672195061bc2ebef7fdc
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 20:11:13 +0200

    add username to twine call

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -6,9 +6,9 @@ if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     python setup.py sdist bdist_wheel
     python -m twine check dist/*
   if [[ $GITHUB_REF =~ -test$ ]]; then
-    python -m twine upload --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
+    python -m twine upload -u oscpy --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
     echo "OOF this would have uploaded"
-    # python -m twine upload --disable-progress-bar dist/*
+    # python -m twine upload -u oscpy --disable-progress-bar dist/*
   fi
 fi
