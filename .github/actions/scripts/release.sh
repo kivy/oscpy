848ef8ac56a3be8de511222141dad13b7214c459
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 21:24:57 +0200

    automatically release on pypi and github when a tag is pushed

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -1,13 +1,52 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
-    pip install -U setuptools wheel twine
-    python setup.py sdist bdist_wheel
-    python -m twine check dist/*
-  if [[ $GITHUB_REF =~ -test$ ]]; then
-    python -m twine upload -u oscpy --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
-  else
-    python -m twine upload -u oscpy --disable-progress-bar dist/*
-  fi
+if [[ ! $GITHUB_REF =~ ^refs/tags/ ]]; then
+   exit 0
 fi
+
+owner=kivy
+repository=oscpy
+access_token=$GITHUB_TOKEN
+
+changelog_lines=$(grep -n "===" CHANGELOG | head -n2 | tail -n1 | cut -d':' -f1)
+
+changelog=$(head -n $(( $changelog_lines - 2)) CHANGELOG | awk '{printf("%s\\n",$0)} END {print ""}')
+
+tag=${GITHUB_REF#refs/tags/}
+draft="false"
+prerelease="false"
+version_name="$tag"
+message="Release $tag"
+
+pip install -U setuptools wheel twine
+python setup.py sdist bdist_wheel
+python -m twine check dist/*
+
+twine="python -m twine upload --disable-progress-bar"
+
+if [[ $GITHUB_REF =~ -test$ ]]; then
+    twine="$twine --repository-url https://test.pypi.org/legacy/"
+
+    draft="true"
+    prerelease="true"
+    message="test release $tag"
+else
+    echo "OOF this would have uploaded"
+    # python -m twine upload --disable-progress-bar dist/*
+    exit 0
+fi
+
+API_JSON="{
+    \"tag_name\": \"$tag\",
+    \"name\": \"$version_name\",
+    \"body\": \"$message\n$changelog\",
+    \"draft\": $draft,
+    \"prerelease\": $prerelease
+}"
+
+echo $API_JSON
+
+$twine dist/*
+curl --data "$API_JSON"\
+     Https://api.github.com/repos/$owner/$repository/releases?access_token=$access_token
--- oscpy/__init__.py
+++ oscpy/__init__.py
@@ -1,3 +1,3 @@
 """See README.md for package information."""
 
-__version__ = '0.6.0-dev'
+__version__ = '0.6.0-dev6'
