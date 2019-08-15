83760203c0422688b6a2e86dd2aedef6ca5eaee3
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-08-15 19:47:29 +0200

    fix tag detection

--- .github/actions/scripts/release.sh
+++ .github/actions/scripts/release.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-if [[ $(git tag | grep v | tail -n 1) = $GITHUB_REF ]]; then
+if [[ "refs/tags/$(git tag | grep v | tail -n 1)" = $GITHUB_REF ]]; then
     pip install -U setuptools wheel twine
     python setup.py sdist bdist_wheel
     twine check dist/*
-  if [[ $GITHUB_REF =~ ^v.*-test$ ]]; then
+  if [[ $GITHUB_REF =~ -test$ ]]; then
     twine upload --disable-progress-bar --repository-url https://test.pypi.org/legacy/ dist/*
   else
     echo "OOF this would have uploaded"
